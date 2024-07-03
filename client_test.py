d734202f07b171b61388e614aa2b5e84674a8b0d
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2024-07-02 23:23:30 -0400

    finished task 1

--- client_test.py
+++ client_test.py
@@ -1,7 +1,8 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
+
   def test_getDataPoint_calculatePrice(self):
     quotes = [
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
@@ -18,7 +19,17 @@ class ClientTest(unittest.TestCase):
 
 
   """ ------------ Add more unit tests ------------ """
-
+  def test_get_ratio(self):
+    ratio_a_1 = 3
+    ratio_a_2 = 4
+    ratio_b_1 = 2
+    ratio_b_bad = 0
+    res_a1_bbad = None
+    res_a1_b1 = (ratio_a_1 / ratio_b_1)
+    res_a2_b_1 = (ratio_a_2 / ratio_b_1)
+    self.assertEqual(getRatio(ratio_a_1,ratio_b_1), res_a1_b1)
+    self.assertEqual(getRatio(ratio_a_2, ratio_b_1), res_a2_b_1)
+    self.assertEqual(getRatio(ratio_a_1, ratio_b_bad), res_a1_bbad)
 
 
 if __name__ == '__main__':
